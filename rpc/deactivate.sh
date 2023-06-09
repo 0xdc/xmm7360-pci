2d373801c1465473f71929186a47ead6dc172e27
Daniel Cordero <xmm7360-pci@0xdc.io>
2023-06-09 06:14:59 +0000

    deactivate: pause between writes

--- rpc/deactivate.sh
+++ rpc/deactivate.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+write1() {
+	test -f "$1" && echo 1 > "$1"
+	sleep 1
+}
+
 ip link set dev wwan0 down
 ip addr flush dev wwan0
 rmmod iosm wwan
 
-echo 1 > "/sys/bus/pci/devices/0000:01:00.0/reset"
-echo 1 > "/sys/bus/pci/devices/0000:01:00.0/remove"
-sleep 1
-
-echo 1 > "/sys/bus/pci/rescan"
-
-modprobe iosm
+write1 "/sys/bus/pci/devices/0000:01:00.0/reset"
+write1 "/sys/bus/pci/devices/0000:01:00.0/remove"
+write1 "/sys/bus/pci/rescan"
 
 until ip link list dev wwan0; do
-	sleep 1
+	sleep 2
 done
