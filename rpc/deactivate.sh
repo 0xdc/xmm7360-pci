f156a918878a61a3b4dc32f8ae549c6eba4da436
Daniel Cordero <xmm7360-pci@0xdc.io>
2024-07-17 08:18:19 +0000

    deactivate: handle modemmanager service

--- rpc/deactivate.sh
+++ rpc/deactivate.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+systemd_mm() {
+	if which systemctl >/dev/null 2>&1 && test "$(systemctl is-enabled ModemManager.service)" = enabled; then
+		if test "$(systemctl is-system-running)" = running; then
+			case "$1" in
+			start)	systemctl start ModemManager.service;;
+			stop)	systemctl stop  ModemManager.service;;
+			esac
+		fi
+	fi
+}
+
 write1() {
 	test -f "$1" && echo 1 > "$1"
 	sleep 1
 }
 
+systemd_mm stop
 ip link set dev wwan0 down
 ip addr flush dev wwan0
 rmmod iosm wwan
@@ -16,3 +28,6 @@ write1 "/sys/bus/pci/rescan"
 until ip link list dev wwan0; do
 	sleep 2
 done
+
+ip link set dev wwan0 up
+systemd_mm start
